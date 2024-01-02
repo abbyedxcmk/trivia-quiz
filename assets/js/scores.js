950b660ef9a0ab8b675b4afbf79a9661c32001f5
Isiaka Bhadmus <swiftnetuk@icloud.com>
2024-01-02 08:19:34 +0000

    Added: Display high scores function

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,1 +1,21 @@
 document.getElementById('clear').addEventListener('click', clearHighScore);
+
+function displayHighScores() {
+    let highScores = localStorage.getItem('highScores');
+    highScores = JSON.parse(highScores);
+    const highScoresTable = document.getElementById('highscores');
+    if (highScores) {
+        for (let i = 0; i <highScores.length; i++) {
+            const li = document.createElement('li');
+            li.innerText = `${highScores[i].userInitials}: ${highScores[i].score}`;
+            highScoresTable.appendChild(li);
+        }
+    } else {
+        document.querySelector('h1').innerText = 'No high score recorded.'
+    }
+}
+
+function clearHighScore() {
+
+}
+displayHighScores();
